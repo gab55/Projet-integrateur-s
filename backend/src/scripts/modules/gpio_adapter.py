ea6e55697fe81d7f10f23c746bba40aff6e6bd41
barbaraestimable@hotmail.com <barbaraestimable@hotmail.com>
2026-09-10 05:00:00 -0400

    Ajout des commentaires finaux pour le fichier gpio_adapter

--- backend/src/scripts/modules/gpio_adapter.py
+++ backend/src/scripts/modules/gpio_adapter.py
@@ -22,7 +22,7 @@ class I2C_screen:
 
         self.lcd.clear()    # Effacer l'écran
 
-    def afficher_message(self, ligne1, ligne2=""):
+    def display_message(self, ligne1, ligne2=""):
 
         # Fonction pour afficher le message sur l'écran
 
@@ -41,50 +41,50 @@ class I2C_screen:
     def cleanup(self):  # Fonction qui efface l'écran
         self.lcd.clear()
 
-class Changement_Etat:  
+class Switch_State:  
 
     # Classe qui gère l'état en passant d'armé à désarmé et de l'alarme active à inactive
 
     def __init__(self):
         # Initialisation de l'état
 
-        self.est_arme =False    # N'est pas armé au début
-        self.est_declenchee = False # L'alarme n'est pas déclenchée au début
+        self.is_armed =False    # N'est pas armé au début
+        self.is_alarming = False # L'alarme n'est pas déclenchée au début
 
     def arm(self):
         # Fonction qui arme 
-        self.est_arme = True
+        self.is_armed = True
         print("Système armé")
 
     def disarm(self):
         # Fonction qui désarme 
-        self.est_arme = False
+        self.is_armed = False
         print("Système déasarmé")
 
-    def alerte_alarm(self):
+    def trigger_alarm(self):
         # Fonction qui active l'alarme
-        self.est_declenchee = True
+        self.is_alarming = True
         print("Alarme déclenchée")
 
-    def arret_alarme(self):
+    def stop_alarm(self):
         # Fonction qui désactive l'alarme 
-        self.est_declenchee = False
+        self.is_alarming = False
         print("Alarme arrêtée")
 
     
 
 
-class Alarme:
+class Alarm:
 
     # Classe qui gère les LED rouge et verte et le buzzer.
 
-    def __init__(self, config, screen=None, changement_etat=None):
+    def __init__(self, config, screen=None, switch_state=None):
 
         # Initialisation GPIO et l'écran
 
         self.gpio = config["gpio"]  # Récupérer les GPIO dans le config.yaml
         self.screen = screen    # Instance de l'écran
-        self.changement_state = changement_etat     # Instance du changement d'état
+        self.switch_state = switch_state     # Instance du switch d'état
 
         self.red_led_pin = self.gpio["red_led_pin"]     # GPIO 5
         self.green_led_pin = self.gpio["green_led_pin"]   # GPIO 22
@@ -115,35 +115,35 @@ class Alarme:
 
     def succes(self):
         # Gère l'armement et le désarmement
-        if self.changement_state.est_arme:
+        if self.switch_state.is_armed:
             # Si le système est armé, on veut le désarmer
-            self.changement_state.disarm()
+            self.switch_state.disarm()
             if self.screen:
-                self.screen.afficher_message("SYSTEME DESARMEE")
+                self.screen.display_message("SYSTEME DESARMEE")
             print("Système désarmée")
         else:
             # Si le système est désarmé, on veut l'armer
-            self.changement_state.arm()
+            self.switch_state.arm()
             GPIO.output(self.green_led_pin, GPIO.HIGH)
             if self.screen:
-                self.screen.afficher_message("SYSTEME ARMEE")
+                self.screen.display_message("SYSTEME ARMEE")
             print("Système armée")
 
             time.sleep(5) # Garde la led verte allumée pendant 5 secondes
 
             GPIO.output(self.green_led_pin, GPIO.LOW)
             if self.screen:
-                self.screen.afficher_message("SYSTEME ARMEE")
+                self.screen.display_message("SYSTEME ARMEE")
 
 
     def activer(self):
 
         # Fonction pour l'activation de l'alarme: la led rouge clignote et le buzzer bip
 
-        self.changement_state.alerte_alarm() # L'état devient déclenchée
+        self.switch_state.trigger_alarm() # L'état devient déclenchée
 
         if self.screen:
-            self.screen.afficher_message("ALARME DECLENCHEE!!")
+            self.screen.display_message("ALARME DECLENCHEE!!")
 
         start_time = time.time() # Enregistre l'heure de départ
 
@@ -161,10 +161,10 @@ class Alarme:
         # Après 5 secondes, led rouge et buzzer sont à OFF
         GPIO.output(self.red_led_pin, GPIO.LOW)
         GPIO.output(self.active_buzzer, GPIO.LOW)
-        self.changement_state.arret_alarme()    # L'état de l'alarme devient arrêtée
+        self.switch_state.stop_alarm()    # L'état de l'alarme devient arrêtée
         
         if self.screen:
-            self.screen.afficher_message("ALARME ARRETEE!")
+            self.screen.display_message("ALARME ARRETEE!")
         print("Alarme arrêtée")
 
     def cleanup(self):
@@ -178,14 +178,14 @@ class MotionSensor:
 
     # Classe qui gère le capteur de détection de mouvement
 
-    def __init__(self, config, screen=None, changement_state=None, alarme=None):
+    def __init__(self, config, screen=None, switch_state=None, alarm=None):
 
         # Initialisation du capteur
 
         self.sensor_pin = config["sensor"]["pir_motion_sensor"] # GPIO 4
         self.screen = screen    # Instance de l'écran
-        self.changement_state = changement_state    # Instance de l'état du système
-        self.alarme = alarme    # Instance de l'alarme
+        self.switch_state = switch_state    # Instance de l'état du système
+        self.alarm = alarm    # Instance de l'alarme
         self.last_state = False     # Le dernier état est à pas de mouvement
         self.state_count = 0    # Compteur
 
@@ -203,66 +203,85 @@ class MotionSensor:
 
         def check_motion():
             while True:
-                # capteur: HIGH= mouvement et LOW= pas de mouvement
+                # Capteur: HIGH= mouvement et LOW= pas de mouvement
                 current_state = GPIO.input(self.sensor_pin) == GPIO.HIGH
 
-                if current_state != self.last_state:
-                    self.state_count += 1
+                # Permet d'éviter les faux positifs quand on utilise le clavier
+                if current_state != self.last_state:    # Si l'état à changer depuis la dernière lecture faite
+                    self.state_count += 1       # Uncrémente le state_count
 
+                    # Permet de confirmer que l'état a été changé au moins 3 fois de suite (pas un faux positifs) 
                     if self.state_count >=3 and current_state != self.last_state:
-                        self.last_state = current_state
-                        self.callback(self.sensor_pin)
-                        self.state_count = 0
+                        self.last_state = current_state     # Pour mettre l'ancien état à jour
+                        self.callback(self.sensor_pin)   # On appelle l'action callback
+                        self.state_count = 0    # On réinitialise le compteur
 
+                # Si l'état est pareille que la dernière fois
                 else:
-                    self.state_count = 0
+                    self.state_count = 0 # Il n'y a pas de changement, on réinitialise le compteur
 
-                time.sleep(0.05)
+                time.sleep(0.05)    # On vérifie le capteur tous les 50 ms
 
+        # On utilise un thread qui s'arrête en même temps que notre code
         thread = threading.Thread(target=check_motion, daemon=True)
-        thread.start()
+        thread.start()  # On démare le thread 
 
     def callback(self, channel):
 
-        if GPIO.input(self.sensor_pin):
+        # C'est la fonction qui est appelé quand le motion sensor détecte le changement d'état
 
-            print("Mouvement détecté!")
+        if GPIO.input(self.sensor_pin):     # Pour détecter si il y a du mouvement
 
-            if self.changement_state.est_arme and not self.changement_state.est_declenchee:
+            print("Mouvement détecté!")     # Si le capteur est HIGH signifie que le mouvement en détecté
+
+            # Si le système doit être armé et si l'alarme n'est pas encore activé
+            if self.switch_state.is_armed and not self.switch_state.is_alarming:
+                # Message afficher sur le moniteur
                 print("Alarme déclenché par mouvement")
+                # Message afficher sur l'écran LCD
                 if self.screen:
-                    self.screen.afficher_message("MOUVEMENT DÉTECTÉ!")
-
-                self.alarme.activer()
+                    self.screen.display_message("MOUVEMENT DÉTECTÉ!")
+                # On déclenche l'alarme
+                self.alarm.activer()
 
         else:
+            # Quand le capteur ne détecte pas de mouvement
 
+            # Message afficher sur le moniteur
             print("Aucun mouvement détecté.")
+            # Message afficher sur l'écran LCD
             if self.screen:
-                self.screen.afficher_message("AUCUN MOUVEMENT")
+                self.screen.display_message("AUCUN MOUVEMENT")
 
 
 class Keypad:
 
+    # classe qui gère le clavier
+    
     def __init__(self, config, screen=None):
 
-        gpio = config["gpio"]
-        self.screen = screen
+        # Initialiser le clavier
 
+        gpio = config["gpio"]   # On récupére les données du gpio du config.yaml
+        self.screen = screen    # Instance de l'afficheur LCD
+
+        # Les lignes sont en sortie
         self.ROW_PINS = [
-            gpio["c1_keypad"],
-            gpio["c2_keypad"],
-            gpio["c3_keypad"],
-            gpio["c4_keypad"]
+            gpio["c1_keypad"],  # ligne 0
+            gpio["c2_keypad"],  # ligne 1
+            gpio["c3_keypad"],  # ligne 2
+            gpio["c4_keypad"]   # ligne 3
         ]
 
+        # Les colonnes sont en entrée
         self.COLUMN_PINS = [
-            gpio["r4_keypad"],
-            gpio["r3_keypad"],
-            gpio["r2_keypad"],
-            gpio["r1_keypad"]
+            gpio["r4_keypad"],  # colonne 0
+            gpio["r3_keypad"],  # colonne 1
+            gpio["r2_keypad"],  # colonne 2
+            gpio["r1_keypad"]   # colonne 3
         ]
 
+        # C'est la matrice des touches
         self.KEYMAP = [
             ["1", "2", "3", "A"],
             ["4", "5", "6", "B"],
@@ -275,82 +294,104 @@ class Keypad:
             GPIO.setmode(GPIO.BCM)
 
         for row in self.ROW_PINS:
-            GPIO.setup(row, GPIO.OUT)
-            GPIO.output(row, GPIO.HIGH)
+            GPIO.setup(row, GPIO.OUT)   # On configure les lignes en sortie
+            GPIO.output(row, GPIO.HIGH) # Les lignes sont tous à HIGH
 
         for col in self.COLUMN_PINS:
+            # On configure tous les colonnes à HIGH et on utilise le pull up down interne
             GPIO.setup(col, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def lire_touche(self):
+    def scan_keys(self):
 
-        for r in range(4):
+        # La fonction scanne toutes les lignes pour détecter les touches qui sont pressées
+
+        for r in range(4): 
+            # Chaque ligne est parcourue (ligne 0, 1, 2, 3)
 
             GPIO.output(self.ROW_PINS[r], GPIO.LOW)
 
             for c in range(4):
+                # Chaque colonne est parcourue (colonne 0, 1, 2, 3)
 
-                if GPIO.input(self.COLUMN_PINS[c]) == GPIO.LOW:
+                if GPIO.input(self.COLUMN_PINS[c]) == GPIO.LOW:     # Une touche a été détectée
 
-                    time.sleep(0.05)
+                    time.sleep(0.05)    # On attend 50 ms
 
+                    # On attend que la touche soit relâcher (passe de LOW à HIGH)
                     while GPIO.input(self.COLUMN_PINS[c]) == GPIO.LOW:
                         pass
 
-                    GPIO.output(self.ROW_PINS[r], GPIO.HIGH)
+                    GPIO.output(self.ROW_PINS[r], GPIO.HIGH)    # On remet la ligne à HIGH
 
-                    return self.KEYMAP[r][c]
+                    return self.KEYMAP[r][c]    # Permet de retourner la touche qui a été pressé
 
-            GPIO.output(self.ROW_PINS[r], GPIO.HIGH)
+            GPIO.output(self.ROW_PINS[r], GPIO.HIGH) # Permet de retouner la ligne à HIGH avant de passer à la prochaine ligne
 
-        return None
+        return None     # Retourne aucune touche si rien n'a été pressée
 
-    def commencer_ecouter(self, callback):
+    def start_listening(self, callback):
 
-        def ecoute():
+        # Fonction qui permet de faire l'écoute en continu avec un autre thread
 
-            derniere_touche = None
+        def listen():
 
-            while True:
+            # Fonction à l'intérieur d'une fonction qui toutrne à l'infini avec while
 
-                touche = self.lire_touche()
+            last_key = None     # On garde la dernière touche pressée en mémoire pour éviter les doublons
 
-                if touche and touche != derniere_touche:
-                    callback(touche)
-                    derniere_touche = touche
+            # Boucle infinie qui scanne le clavier en continue
+            while True:   
 
-                elif not touche:
-                    derniere_touche = None
+                key = self.scan_keys()  # Pour lire une touche pressée
 
-                time.sleep(0.05)
+                if key and key != last_key:     # Si une nouvelle touche a été pressée
+                    callback(key)   # On appelle la fonction callback avec la touche
+                    last_key = key  # On mémorise la touche pressé comme la dernière
 
+                elif not key:   # Si aucune touche a été pressée
+                    last_key = None     # On réinitialise 
+
+                time.sleep(0.01) # On scanne le clavier tous les 10 ms
+
+        # On créer un thread qui s'arrête quand le programme s'arrête
         thread = threading.Thread(
-            target=ecoute,
-            daemon=True
+            target=listen,  # C'est la fonction qu'on utilise
+            daemon=True     # Le mode
         )
 
+        # On démarre le thread
         thread.start()
 
 
 class Controle_Acces:
 
+    # Classe qui gère le PIN pour armer ou désarmer l'alarme
+
     def __init__(self, password):
 
-        self.password = password
-        self.code = ""
+        # Initialise avec le bon mot de passe
 
-    def ajouter(self, touche):
+        self.password = password    # On stocke le bon mot de passe
+        self.code = ""      # On stocke que l'utilisateur va pressé
 
-        self.code += touche
+    def add(self, key):
 
-    def effacer(self):
+        # Fonction qui permet d'ajouter une touche pressée au code
 
-        self.code = ""
+        self.code += key    # Permet d'ajouter la touche pressée à la fin de chaque touche saisie
 
-    def verifier(self):
+    def erase(self):
+
+        # La fonction efface le code entré au complet
+
+        self.code = ""  
+
+    def verify(self):
+
+        # La fonction vérifie si c'est le bon code qui a été saisie
 
         resultat = self.code == self.password
-        self.code = ""
+        self.code = ""  # Efface le code en mémoire
 
-        return resultat
-
+        return resultat     # Retourne Tue ou False dépendemment du résultat de la vérification
 
