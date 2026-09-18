9081d1104cadcc717e3872a1fd5f830419667d82
gab55 <108195336+gab55@users.noreply.github.com>
2026-09-18 16:35:15 -0400

    Hotfix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -10,17 +9,10 @@ const sensorRoutes = require("./routes/sensor.routes");
 const logRoutes = require("./routes/log.routes");
 const alertRoutes = require("./routes/alert.routes");
 const metricsRoutes = require("./routes/metrics.routes");
+const alarmRoutes = require('./routes/alarm.routes');
+const rpiRoutes = require('./routes/rpi.routes');
 
 const { logAction } = require("./services/log.service");
-
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("MongoDB connecté");
-    })
-    .catch(err => {
-        console.error("Erreur MongoDB:", err);
-        process.exit(1);
-    });
 
 app.use(cors());
 app.use(express.json());
@@ -29,9 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
-const sensorRoutes = require("./routes/sensor.routes");
-const alarmRoutes = require('./routes/alarm.routes');
-const rpiRoutes = require('./routes/rpi.routes');
+
 app.use("/api/auth", authRoutes);
 app.use("/api/logs", logRoutes);
 
